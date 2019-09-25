63be0297d581f1e2e49a218f1d72bc5ff988b2a6
SimonDanisch <sdanisch@gmail.com>
2019-09-25 19:30:32 +0200

    saver channel etc

--- src/ReactiveRuntime.jl
+++ src/ReactiveRuntime.jl
@@ -14,14 +14,33 @@ function Base.show(io::IO, m::MIME"application/vnd.webio.application+html", x::O
 end
 
 
-const cell_outputs = Set{Symbol}()
+const cell_outputs = Dict{Symbol, Any}()
+const obs_func_names = Dict{Symbol, Symbol}()
+const channels = Dict{Symbol, Channel}()
 
 function collect_globals!(globals, expr, current)
     vars = Set{Symbol}()
     MacroTools.postwalk(expr) do var
-        var isa Symbol && var != current && var in globals && push!(vars, var)
+        var isa Symbol && var != current && haskey(globals, var) && push!(vars, var)
     end
     return vars
+end
+
+struct OnUpdater{F}
+    f::F
+    name::Symbol
+    observable::Observable{Any}
+end
+
+function (f::OnUpdater)(val)
+    newval = f.f()
+    if haskey(channels, f.name)
+        close(pop!(channels, f.name))
+    end
+    if newval isa Channel
+        channels[f.name] = newval
+    end
+    f.observable[] = newval
 end
 
 function lift_cell_reactive(expr)
@@ -36,24 +55,38 @@ function lift_cell_reactive(expr)
     end
     obs_vars = collect_globals!(cell_outputs, body, var)
     varname = var === nothing ? gensym("tmp") : var
-    varesc = (varname)
+    funcname = get!(obs_func_names, varname, gensym(varname))
+    var_quote = QuoteNode(varname)
+    func_quote = QuoteNode(funcname)
     lifted = quote
-        function f($(obs_vars...))
-            $(body)
-        end
-        if isdefined(Main, $(QuoteNode(varname)))
-            $(varesc)[] = f(getindex.(($(obs_vars...),))...)
-        else
-            $(varesc) = ReactiveRuntime.Observable(f(getindex.(($(obs_vars...),))...))
-            ReactiveRuntime.onany($(obs_vars...)) do args...
-                $(varesc)[] = f(args...)
+        $(varname) = let
+            function $(funcname)($(obs_vars...))
+                $(body)
             end
+            new_parents = ($(obs_vars...),)
+            observable, old_parents = get!(ReactiveRuntime.cell_outputs, $(var_quote)) do
+                ReactiveRuntime.Observable{Any}(nothing), new_parents
+            end
+            observable, old_parents = ReactiveRuntime.cell_outputs[$(var_quote)]
+            new_updater = ReactiveRuntime.OnUpdater($(func_quote), observable) do
+                 Base.invokelatest($(funcname), getindex.(new_parents)...)
+            end
+            # remove old listeners
+            for old_parent in old_parents
+                filter!(old_parent.listeners) do callback
+                    !(callback isa ReactiveRuntime.OnUpdater && callback.name == $(func_quote))
+                end
+            end
+            # add new listeners
+            for new_parent in new_parents
+                ReactiveRuntime.on(new_updater, new_parent)
+            end
+            # call the updater, to update value with the new function body
+            new_updater(nothing)
+            observable
         end
-    		$(varesc)
     end
-    if var !== nothing
-        push!(cell_outputs, var)
-    end
+
     return lifted
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using ReactiveRuntime
 using Test
+using ReactiveRuntime: @cell
 
-@testset "ReactiveRuntime.jl" begin
-    # Write your own tests here.
+
+@cell test = begin
+    77
 end
+
+@cell test3 = begin
+    "1 + 44"
+end
+@cell test2 = begin
+    string(test * 20, " ", test3)
+end
+
+ReactiveRuntime.on(println, test2)
